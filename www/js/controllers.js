119dfb341d7efda6e082a770f444d56f29dda122
Stefan Jansen <sja@blaud.com>
2016-09-09 11:59:14 +0200

    send

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,6 +12,8 @@ angular.module('starter.controllers', [])
         cameraService.takePhoto().then(function(photo){
             console.log("success", arguments);
             itemService.addItem(photo);
+            alert("picture saved, sending to server");
+
         },function(){
             alert("ERROR!");
             console.error("error", arguments);
@@ -19,7 +21,7 @@ angular.module('starter.controllers', [])
     };
 })
 
-.controller('picturesCtrl', function ($scope, itemService, imageService, $timeout, $http) {
+.controller('picturesCtrl', function ($scope, itemService, $http, fileService, $cordovaDevice, $q) {
     /**
      * get all items saved in localstorage using itemService
      */
@@ -30,22 +32,66 @@ angular.module('starter.controllers', [])
      * @param picture
      */
     $scope.toggle = function (picture) {
+        for(var i=0; i<$scope.pictures.length; i++){
+            if(picture === $scope.pictures[i]) continue;
+            $scope.pictures[i].checked = false;
+        }
+
         if(picture){
             picture.checked = !picture.checked;
         }
     };
 
     $scope.sendSelected = function () {
-        var items = $scope.pictures.filter(function (picture) {
-            return picture.checked;
-        });
-        console.log("items: ", items);
+        var defer = $q.defer();
 
-        if(items.length>0){
-            alert("sending " + items.length + " items.");
+        var picture = $scope.pictures.filter(function (picture) {
+            return picture.checked;
+        })[0];
+
+        if(picture){
+            var req = {
+                method: "",
+                url: "http://mia-preview.azurewebsites.net/api/foto/",
+                data: {zaakId: 1},
+                headers:[]
+            };
+
+            req.headers['Accept'] = "application/json";
+            req.headers['X-S-Username'] = "DummyUserName_AppIsNotGoodEnabled";
+            req.headers['X-S-UserEmailAddress'] = "DummyUserEmailAddress_AppIsNotGoodEnabled";
+            req.headers['X-S-Platform'] = $cordovaDevice.getPlatform();
+            req.headers['X-S-PlatformVersion'] = $cordovaDevice.getVersion();
+            req.headers['X-S-AppName'] = "DummyAppName";
+            req.headers['X-S-AppVersion'] = "DummyVersie";
+            req.headers['Authorization'] = "Basic " + btoa("user:superSecr");
+
+            var filename = picture.title;
+            if (picture.filenameFull.lastIndexOf(".") !== -1) {
+                filename = picture.title + picture.filenameFull.substr(picture.filenameFull.lastIndexOf("."));
+            }
+
+
+            var options  = {
+                fileKey: "foto",
+                fileName: filename,
+                params : req.data,
+                headers: req.headers
+            };
+
+
+            fileService.upload(picture.filenameFull, req.url, options).then(function(result){
+                console.log("result", result);
+                defer.resolve(result);
+            }, function(err) {
+                console.log("err", err);
+                defer.reject(err);
+            });
         }else{
             alert("no items selected.");
         }
+
+        return defer.promise;
     }
 })
 ;
