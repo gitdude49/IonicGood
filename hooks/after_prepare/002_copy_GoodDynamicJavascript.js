ce827f6221e322d24d990ab00a81aa481e5ae8dd
USchoo <urbaan@blaud.com>
2016-09-21 17:44:03 +0200

    Using GoodDynamics.js from version v2.4.0.99

--- hooks/after_prepare/002_copy_GoodDynamicJavascript.js
+++ hooks/after_prepare/002_copy_GoodDynamicJavascript.js
@@ -5,7 +5,7 @@ console.log("- running hook script: " + __filename);
 
 var fs = require("fs-extra");
 
-var src = "./Good.SDK/GoodPluginOriginal/GoodDynamics.js";
+var src = "./Good.SDK/GoodPluginOriginal/Good_Dynamics_for_PhoneGap_v2.4.0.99/iOS/Source/GoodDynamics.js";
 var dest = "./platforms/ios/www/GoodDynamics.js"
 fs.copySync(src, dest);
 
